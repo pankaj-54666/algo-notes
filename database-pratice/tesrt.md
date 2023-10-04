dc15375afd0656db1d6d0c1e021af831d7bd73d0
Pankaj Kumar <Pankaj.54666@gmail.com>
2023-10-04 07:12:20 +0530

    tasklist update

--- database-pratice/tesrt.md
+++ database-pratice/tesrt.md
@@ -1,3 +1,6 @@
+# docker setup
+option1: spin-up a docker instance of postgresql to test on terminal.
+option2: install postgresql on corresponding os.
 
 # table creation
 
@@ -30,7 +33,7 @@ cascade delete for fk.
 write out mybatis code for : insertion,deletion and update.
 in mybatis return appropriate iteams after operation.
 
-# mastering search
+# mastering search1
 
 search from employee table, who have kumar as their last name.
 search from employee and department table to print the department of ramesh.
@@ -47,6 +50,55 @@ use GROUP BY to get all exercise in a workout as commma seperated string. (hint:
 give one example each where: left join is required, right join is required.
 
 using GROUP BY and MAX, find MAX salary of employee.
+
+# exercise1
+TODO: list down leetcode and other problems which you want to pratice later here.
+
+# mastering search2
+
+Use of HAVING with GROUP BY : having helps filtering after the group by has been done.
+operators in query: (+,-,*,/)
+window function
+aggregator functions : MAX,SUM,MIN,AVG
+GROUP BY : Date(month, year)
+
+useful functions: ROUND
+
+
+# mastering subquery
+
+helpful for cleaner query: 
+## subquery : query inside another query
+subquery can go at various places like in the FORM clause(works like a table), in the WHERE clause (with operator, IN keyworks)
+
+subquery in WHERE
+    - subquery with single row
+    - subquery with multiple rows
+    - subquery with multiple column
+
+subquery in FROM clause
+
+subquery in SELECT clause
+
+nested query vs correlated query 
+> a correlated query is a type of nested query, where the outer query cannot run indepedently of the inner query.
+
+
+    
+
+## views
+
+
+
+userful keyworks: LIMIT
+
+# exercise2
+currency conversion problem from ms3 (l05)
+
+
+# triggers
+
+#
 
 
 
--- database-pratice/test.sql
+++ database-pratice/test.sql
@@ -9,3 +9,140 @@ INNER JOIN workout_exercise we ON w.id = we.workoutid
 WHERE w.id = $workoutid$
 GROUP BY(w.id,w.name)
 ORDER BY we.sequence_no
+
+-- Maximum Salary
+SELECT MAX(salary)
+FROM employee
+
+SELECT MAX(salary)
+FROM employee
+GROUP BY department
+
+
+-- subquery example1 returning single row
+SELECT book_id, title, isbn13, language_id, num_pages, publication_date, publisher_id
+FROM book
+WHERE num_pages > (
+  SELECT AVG(num_pages)
+  FROM book
+);
+
+-- subquery with multiple row return
+SELECT
+book_id,
+title
+FROM book
+WHERE language_id IN (
+  SELECT language_id
+  FROM book_language
+  WHERE language_code IN ('eng', 'en-GB', 'en-CA', 'en-US')
+);
+
+-- subquery with multiple column
+SELECT
+co.order_id,
+co.order_date,
+co.customer_id,
+a.address_id,
+a.street_number,
+a.street_name,
+a.city,
+c.country_name
+FROM cust_order co
+INNER JOIN address a ON co.dest_address_id = a.address_id
+INNER JOIN country c ON a.country_id = c.country_id
+WHERE (a.city, c.country_name) IN (
+  SELECT
+  a.city,
+  co.country_name
+  FROM address a
+  INNER JOIN customer_address ca ON a.address_id = ca.address_id
+  INNER JOIN country co ON a.country_id = co.country_id
+  WHERE ca.customer_id = 26
+);
+
+-- subquery selecting details of book with maximum price
+SELECT
+title, 
+isbn13,
+publication_date
+FROM book
+WHERE book_id IN (
+	SELECT book_id
+	FROM order_line
+	WHERE price = (
+		SELECT MAX(price)
+		FROM order_line
+	)
+);
+
+-- correlated query (We want to see books that are longer, or have a higher number of pages, than the average number of pages for that book’s publisher.)
+-- same can be written using joins
+SELECT
+book_id,
+title,
+num_pages,
+publisher_id
+FROM book b1
+WHERE num_pages > (
+  SELECT
+  AVG(num_pages)
+  FROM book b2
+  WHERE b2.publisher_id = b1.publisher_id
+);
+
+-- subquery + select1 (get overall averge column)
+SELECT
+book_id,
+title,
+num_pages,
+publisher_id,
+(
+  SELECT AVG(num_pages)
+  FROM book
+) AS overall_avg
+FROM book;
+
+-- subquery + select2 (get average of each publisher)
+SELECT
+book_id,
+title,
+num_pages,
+publisher_id,
+(
+  SELECT AVG(b2.num_pages)
+  FROM book b2
+  WHERE b2.publisher_id = b1.publisher_id
+) AS avg_for_publisher
+FROM book b1;
+
+-- subquery + having
+SELECT
+CONVERT(date, order_date) AS order_date_only,
+COUNT(*)
+FROM cust_order
+GROUP BY CONVERT(date, order_date)
+HAVING COUNT(*) >= (
+	SELECT COUNT(*)
+	FROM cust_order
+	WHERE CONVERT(date, order_date) = '2020-01-01'
+)
+ORDER BY CONVERT(date, order_date) ASC;
+
+
+-- subquery + select
+INSERT INTO order_summary (order_date, order_count)
+VALUES ('2021-01-01', (
+  SELECT COUNT(*)
+  FROM cust_order
+  WHERE DATE(order_date) = '2021-01-01'
+  )
+);
+
+-- add a comment to a book if it was published after the very first order we created in our system.
+UPDATE book b
+SET b.comments = (
+  SELECT COUNT(*)
+  FROM order_line o
+  WHERE o.book_id = b.book_id
+);
